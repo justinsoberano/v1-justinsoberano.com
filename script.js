ada70c342f75c1e11dd18afb3ead041b92bff21f
Justin Soberano <jsoberano2002@gmail.com>
2023-02-13 00:28:29 -0600

    added changes

--- script.js
+++ script.js
@@ -22,35 +22,57 @@ const axis = new THREE.AxesHelper(5);
 scene.add(axis);
 
 // creates the yellow plane 
-const planeMesh = new THREE.PlaneGeometry(10, 10);
-const planeMaterial = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide});
+const planeMesh = new THREE.PlaneGeometry(1000, 1000);
+const planeMaterial = new THREE.MeshBasicMaterial({color: 0x4D4D4D, side: THREE.DoubleSide});
 const plane = new THREE.Mesh(planeMesh, planeMaterial);
 plane.rotation.x = Math.PI / 2;
 plane.position.y = -3;
 scene.add(plane);
 
-const mesh = new THREE.SphereGeometry(1, 16, 16);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const sphere = new THREE.Mesh(mesh, material);
+// const mesh = new THREE.SphereGeometry(1, 16, 16);
+// const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+// const sphere = new THREE.Mesh(mesh, material);
 
-scene.add(sphere);
+// scene.add(sphere);
 
-const mesh2 = new THREE.BoxGeometry(1, 1, 1);
-const material2 = new THREE.MeshBasicMaterial({color: 0x0000ff});
-const cube = new THREE.Mesh(mesh2, material2);
+// Universal Cube Mesh for the Cubes
+const cubeMesh = new THREE.BoxGeometry(1, 1, 1);
+
+// Material and Constructor for Cube1
+const material2 = new THREE.MeshBasicMaterial({color: 0x493DD1});
+const cube = new THREE.Mesh(cubeMesh, material2);
 cube.position.set(1, 1, 1);
 scene.add(cube);
 
+// Material and Constructor for Cube2
+const material3 = new THREE.MeshBasicMaterial({color: 0x78AA51});
+const cube2 = new THREE.Mesh(cubeMesh, material3);
+cube2.position.set(-5, 1, 3);
+scene.add(cube2);
+
+// Dodecahedron Mesh
+const shapeMesh = new THREE.DodecahedronGeometry(1, 0);
+const shapeMaterial = new THREE.MeshBasicMaterial({color: 0xAE14CB});
+const dodecahedron = new THREE.Mesh(shapeMesh, shapeMaterial);
+dodecahedron.position.set(-1, -2, 2);
+scene.add(dodecahedron);
+
+// doesnt work? needs shaders probably
 const light = new THREE.AmbientLight(0x404040); // soft white light
 light.position.set(1, 1, 1);
 scene.add(light);
 
-function animation() {
+function animateCubes() {
     cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cube.rotation.z += 0.01;
+    cube.rotation.y += 0.02;
+    cube.rotation.z += 0.02;
+    cube2.rotation.x += 0.01;
+    cube2.rotation.y += 0.01;
+    cube2.rotation.z += 0.03;
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+    dodecahedron.rotation.z += 0.01;
     renderer.render(scene, camera);
 }
-// renders the scene
 
-renderer.setAnimationLoop(animation);
+renderer.setAnimationLoop(animateCubes);
